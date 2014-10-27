47b04b4aecc63ceeb9f06182743c7479aaf2f0c4
kamelzcs <kamelzcs@gmail.com>
2014-10-27 22:19:25 +0900

    add install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+#
+# install.sh
+# Copyright (C) 2014 zhao <zhao@kamel-ThinkPad-X201>
+#
+# Distributed under terms of the MIT license.
+#
+
+ln -s ~/dotfile/.bashrc ~/.bashrc
+ln -s ~/dotfile/.zshrc ~/.zshrc
+ln -s ~/dotfile/.fonts ~/.fonts
+
+
+
