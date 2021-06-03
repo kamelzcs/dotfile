52a18d7fa34336e412a91eeb21c6a60dcade7f27
szhao <szhao@indeed.com>
2021-06-03 11:32:26 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -6,9 +6,9 @@
 # Distributed under terms of the MIT license.
 #
 
-ln -s ~/dotfile/.bashrc ~/.bashrc
+# ln -s ~/dotfile/.bashrc ~/.bashrc
 ln -s ~/dotfile/.zshrc ~/.zshrc
-ln -s ~/dotfile/.fonts ~/.fonts
-
+# ln -s ~/dotfile/.fonts ~/.fonts
+ln -s ~/dotfile/.vimrc ~/.vimrc
 
 
